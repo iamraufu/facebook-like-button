2f4199787569a560298594f84c953ee0affc282d
Eftykhar Rahman <eftykharraufu@gmail.com>
2021-03-15 09:17:34 +0600

    like button color added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
-
+import ThumbUpAltIcon from '@material-ui/icons/ThumbUpAlt';
+import { useState } from 'react';
 function App() {
+  const [likeColor, setLikeColor] = useState('');
+  const [icon,setIcon] = useState()
   return (
     <div className="App">
       <header className="App-header">
-       
+        <ThumbUpAltIcon onClick={() => {
+          const color = likeColor ? '' : 'primary'
+          setLikeColor(color)
+        }} color={likeColor}></ThumbUpAltIcon>
       </header>
     </div>
   );
